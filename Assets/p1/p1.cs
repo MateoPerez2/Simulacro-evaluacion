2739e53a4c0345d36e45726c9e0111718b28e185
MateoPerez2 <mateopaparella@hotmail.com>
2022-04-08 11:41:56 -0300

    Exercise 1 and 2 completed

---
+++ Assets/p1/p1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p1 : MonoBehaviour
+{
+    public float valor1;
+    public float valor2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (valor1 == valor2)
+        {
+            Debug.Log("son iguales");
+        }
+        else
+        {
+            Debug.Log("no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/p2/p2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p2 : MonoBehaviour
+    
+{public float producto1;
+    public float producto2;
+    public float producto3;
+    public float montoDisponible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (montoDisponible > producto1 + producto2 + producto3)
+        {
+            Debug.Log("La suma de los productos no supera el monto disponible");
+        }
+        if (montoDisponible < producto3 + producto2 + producto1)
+        {
+            Debug.Log("El monto disponible no es suficiente");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
